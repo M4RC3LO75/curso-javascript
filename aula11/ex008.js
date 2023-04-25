f509b08626b379fcfb77c893d1a033c4fba2dd59
M4RC3LO75 <126880453+M4RC3LO75@users.noreply.github.com>
2023-04-25 17:17:33 -0300

    aula 11 created

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade.`)
+}
+ else{
+    console.log(`Você estava dentro do limite de velocidade.`)
+ }
+
+ console.log('Dirija sempre usando o cinto de segurança.')
