d35c15ef8cb155dd7fda5649e0165820859ee3e4
M4RC3LO75 <126880453+M4RC3LO75@users.noreply.github.com>
2023-05-04 09:31:35 -0300

    ex017 improved

--- aula14ex/ex017/JS/script.js
+++ aula14ex/ex017/JS/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const inputvalue = document.querySelector('#inumber');
-const resultText = document.getElementById('result')
+const resultBox = document.getElementById('result-box')
 let calcButton = document.querySelector('#calc-button')
 
 function calculate() {
     var number = Number(inputvalue.value);
-    resultText.innerText = '';
+    resultBox.innerText = '';
     for (var c = 1; c <= 10; c++) {
         var result = c * number;
-        resultText.innerHTML += `${c} x ${number} = ${result}</br>`;
+        resultBox.innerHTML += `<div class="result-line">${c} x ${number} = ${result}</div>`;
         console.log(number);
     }
 }
