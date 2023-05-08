b0aaa92ee8f3c96db7581917253df1c1b0f2d4ca
Marcelo Bastos <marcelobastos1975@gmail.com>
2023-05-08 19:32:15 -0300

    Update ambient.js

--- aula15/ambient.js
+++ aula15/ambient.js
@@ -1,3 +1,13 @@
-let num = [5,8,6]
-num.push('fim')
-console.log(`Nosso vetor é ${num}`);
+let num = [5,8,6,9,1,3]
+num.push(2);
+num.sort();
+for (let i in num) {
+    console.log(`O valor do vetor ${i} é ${num[i]}`);
+}
+let pos = num.indexOf(7);
+
+if (pos == -1) {
+    console.log('Valor não encontrado')
+} else {
+    console.log(`O Número 8 está na posição: ${pos}`);
+}
